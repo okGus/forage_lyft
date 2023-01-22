fef48aad1f4055e899f4791daa7d627123948dff
Gustavo <rosalesagustavo@gmail.com>
2023-01-22 18:04:45 -0500

    tire to rorschach model car

--- engine/model/rorschach.py
+++ engine/model/rorschach.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 from engine.willoughby_engine import WilloughbyEngine
 from batterys.nubbin_battery import NubbinBattery
+from tire.carrigan import Carrigan
 from dataclasses import dataclass
 
 @dataclass
@@ -9,10 +10,11 @@ class Rorschach:
     """Model of Car"""
     engine: WilloughbyEngine
     battery: NubbinBattery
+    tire: Carrigan
 
     def needs_service(self) -> bool:
         service_threshold_date = self.battery.last_service_date.replace(year=self.battery.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine.engine_should_be_serviced():
+        if service_threshold_date < datetime.today().date() or self.engine.engine_should_be_serviced() or self.tire.tire_should_be_serviced():
             return True
         else:
             return False
