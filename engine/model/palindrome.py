50bede4b7060cbf4ac85a0f410ad9210c32f9c1e
Gustavo <rosalesagustavo@gmail.com>
2023-01-22 18:13:25 -0500

    tire to palindrome model car

--- engine/model/palindrome.py
+++ engine/model/palindrome.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from datetime import datetime
 from engine.sternman_engine import SternmanEngine
 from batterys.spindler_battery import SpindlerBattery
+from tire.octoprime import Octoprime
 from dataclasses import dataclass
 
 @dataclass
@@ -11,10 +12,11 @@ class Palindrome:
     """Model of Car"""
     engine: SternmanEngine
     battery: SpindlerBattery
+    tire: Octoprime
 
     def needs_service(self):
         service_threshold_date = self.battery.last_service_date.replace(year=self.battery.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine.engine_should_be_serviced():
+        if service_threshold_date < datetime.today().date() or self.engine.engine_should_be_serviced() or self.tire.tire_should_be_serviced():
             return True
         else:
             return False
